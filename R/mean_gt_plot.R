4ead41ce9174d2367c6281043292fcb713cfe31c
Mahdi <imahdimir@gmail.com>
2024-06-03 12:28:32 -0700

    Full Sibs Mean Genotype Correleation by INFO Score

--- R/mean_gt_plot.R
+++ R/mean_gt_plot.R
@@ -1,13 +1,14 @@
 #!/usr/bin/env Rscript
 
 library("data.table")
-library("dplyr")
+library(dplyr)
 library("plinkFile")
 library("genio")
 library("ggplot2")
 library("glue")
 library('readxl')
 library(writexl)
+
 
 
 dta_fn <- '/Users/mmir/Library/CloudStorage/Dropbox/corrs.xlsx'
@@ -44,6 +45,47 @@ ofn <- '/Users/mmir/Library/CloudStorage/Dropbox/C1-P-G/A1-Git-SF/summary-stats-
 
 write_xlsx(dta, ofn)
 
+
+
+# only sib_pairs, june 3, 2024 email to Alex
+
+
+dta_fn <- '/Users/mmir/Library/CloudStorage/Dropbox/corrs.xlsx'
+
+dta <- read_excel(dta_fn)
+
+dta <- dta %>% filter(InfType == 'FS')
+
+colnames(dta)[3] <- 'Info'
+
+dta$se = dta$std / sqrt(dta$n_snps)
+dta$Info = dta$Info / 100
+
+l1 <- c("Dosages", 'Hard-Calls')
+
+
+
+# plot correlations
+ggplot(dta, aes(x = Info, y = mean, color=Genotype)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=mean-1.96*se, ymax=mean+1.96*se), width=.01) +
+  geom_line() +
+  geom_hline(yintercept=0.5, linetype="dashed") +
+  scale_color_discrete(labels=l1) +
+  scale_y_continuous(breaks = c(.4, .42, .44, .46,.48, .5)) + 
+  theme_classic() +
+  labs(title = 'Full Sibs Mean Genotype Correleation by INFO Score', x = "INFO Score", y = "Full Sibs Mean Genotype Correlation")
+
+
+
+ofn <- '/Users/mmir/Library/CloudStorage/Dropbox/sibs_mean_gt_corr_by_info_score.png'
+
+ggsave(ofn)
+
+
+ofn <- '/Users/mmir/Library/CloudStorage/Dropbox/C1-P-G/A1-Git-SF/summary-stats-plot-4-pairs-corr-240405-SF/out/corrs.xlsx'
+
+write_xlsx(dta, ofn)
 
 
 
